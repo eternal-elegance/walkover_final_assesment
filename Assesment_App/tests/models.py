260a42da311bef02742e0ab7d009b3221505a0f8
rick_225 <anurag.banerjee011@gmail.com>
2021-11-05 23:45:49 +0530

    created django-project, added base html and designed django models(databases)

---
+++ Assesment_App/tests/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+DIFF_CHOICES = (
+    ('simple', 'simple'),
+    ('moderate', 'moderate'),
+    ('tuff', 'tuff'),
+)
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    topic = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    required_score_to_pass = models.IntegerField(help_text="required score in %")
+    difficulty = models.CharField(max_length=10, choices=DIFF_CHOICES)
+
+    def __str__(self):
+        return f"{self.name}-{self.topic}"
+
+    def get_questions(self):
+      return self.questions.all()
