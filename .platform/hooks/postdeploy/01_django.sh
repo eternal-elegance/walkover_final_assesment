4b92226d194dc1714c218e4de54ae1f9a546a0e3
rick_225 <anurag.banerjee011@gmail.com>
2021-11-07 13:39:19 +0530

    changed commands in .sh file

--- .platform/hooks/postdeploy/01_django.sh
+++ .platform/hooks/postdeploy/01_django.sh
@@ -6,9 +6,9 @@ source /var/app/venv/*/bin/activate && {
 python manage.py collectstatic --noinput;
 # log which migrations have already been applied
 python manage.py showmigrations;
+# migrate user model
+python manage.py migrate users --noinput;
 # migrate the rest
 python manage.py migrate --noinput;
-# another command to create a superuser (write your own)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('walkover', 'walkover@mail.com', 'walkover@1234')" | python manage.py shell
 
 }
